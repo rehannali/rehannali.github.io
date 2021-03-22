27c8badeec167516b007aaf356af3647f3f90f20
Rehan Ali <rehannali@ntpu.info>
2021-03-22 18:43:50 +0500

    Revert reddit and discord changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e6af4538251282cda9d488aa28386db2",
-"index.html": "afbd12f842cf7c19359172fa101470e8",
-"/": "afbd12f842cf7c19359172fa101470e8",
-"main.dart.js": "33088b54854fb113fbb1ec1bbf6cfa3e",
+"index.html": "088a2d908b5d010610f20c1fc1e5d8c8",
+"/": "088a2d908b5d010610f20c1fc1e5d8c8",
+"main.dart.js": "d3d3bb66d4f78493ea468bef5ed08086",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
